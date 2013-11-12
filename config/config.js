16c176e70b937a55ab350e8479a8cdab8101238e
zld <zhu.lida.cn@gmail.com>
2013-11-12 21:25:53 +0800

    little info changed

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@ module.exports = {
 /*
 * 注意，所有的路径配置的前面请加‘/’而后面不要加'/'！
 */
-    server: 'angularjs.cn',
+    server: 'gtd-club.com',
     poweredBy: 'jsGen',
     listenPort: 3000,//监听端口，如果配合clusterplus监听多个端口，这里也可以使用[3000, 3001, 3002, 3003]数组形式，rrestjs会智能分析
     //baseDir: path.join(__dirname, '/..'), //绝对目录地址，下面的目录配置都是根据这个目录的相对地址，这里是根据config文件进行配置地址
